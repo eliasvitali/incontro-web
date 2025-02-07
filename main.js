eff2de9f244bf335967be8861905c561c64984c3
Aaron <38788426+derochedesign@users.noreply.github.com>
2025-02-07 02:02:18 -0500

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+let root = document.documentElement;
+
+document.addEventListener("DOMContentLoaded", _ => {
+    
+
+    
+    //get data
+    // $.getJSON('../data/x.json', function(result) { 
+           
+    // });
+    
+    
+});
+
+//all click events
+document.addEventListener("click", e => {
+    
+    //if matches
+    
+});
